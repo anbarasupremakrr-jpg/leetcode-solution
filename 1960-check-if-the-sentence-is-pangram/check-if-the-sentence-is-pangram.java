2c1e56f745c3199a6db7c85ab579cbdee76ce8e0
DHINAKAR A <anbarasupremakrr@gmail.com>
2026-09-22 20:12:00 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (68.75%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean checkIfPangram(String s) {
+        for(char ch='a';ch<='z';ch++){
+            if(s.indexOf(ch)==-1){
+                return false;
+            }
+        }
+        return true;
+    }
+}
